02dbacd7b8c5c75f73d26ed9795ba712ac3ded94
liz <lz9163@163.com>
2016-01-25 12:55:12 +0800

    a
    Signed-off-by: liz <lz9163@163.com>

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World, My neme is Frank! ");
+  response.write("Hello Hello World, My neme is Frank! ");
   response.end();
 }).listen(8888);
